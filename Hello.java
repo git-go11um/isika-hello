efb3f4d182ada0c1cbd9077af56794aee8dd326e
go11um <v.haumont@gmail.com>
2025-06-19 11:27:39 +0200

    v1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main (String []args){
-        System.out.println("Hello from Jenkins!!! - v1");
+        System.out.println("Hello from Jenkins!!! - v2");
     }
 }
